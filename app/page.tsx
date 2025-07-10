2da1d9ec724398cc437f6c2d7af93f2598f129b9
Anish Kancherla <anish.kancherla@gmail.com>
2025-07-09 17:17:08 -0700

    changing main-page ippodo title

--- app/page.tsx
+++ app/page.tsx
@@ -442,7 +442,7 @@ export default function LandingPage() {
               <div className="relative">
                 <div className="mb-6">
                   <h3 className="text-2xl font-bold text-[#1a1a1a] mb-6 font-diatype">
-                    Ippodo Tea Co.
+                    Ippodo Tea
                   </h3>
                   <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-2 mb-6">
                     <span className="text-[#757575] font-diatype-thin">Ummon</span>
